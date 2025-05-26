2f5499029969c34a657155085ba7c577b3f76c5e
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2025-05-25 19:07:22 -0500

    Cambie el link de whasap en el componente

--- src/components/whatsapp.jsx
+++ src/components/whatsapp.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsappFixedIcon() {
   return (
     <a
-      href="https://api.whatsapp.com/message/LL4KM7NN5MTVE1?autoload=1&app_absent=0" // Cambia por tu número real con código internacional
+      href="https://wa.me/573001112233?text=Hola%2C%20quiero%20más%20información" // Cambia por tu número real con código internacional
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 bg-green-600 hover:bg-green-700 text-white rounded-full p-4 shadow-lg z-50 flex items-center justify-center"
